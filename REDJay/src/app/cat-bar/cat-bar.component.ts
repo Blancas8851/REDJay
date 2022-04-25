3d3297fbd21a9e46b4758ac60fd765bf2ffbbc51
Zem77 <dariuswalker878@gmail.com>
2022-04-25 17:47:29 -0400

    first commit

---
+++ REDJay/src/app/cat-bar/cat-bar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-bar',
+  templateUrl: './cat-bar.component.html',
+  styleUrls: ['./cat-bar.component.css']
+})
+export class CatBarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
