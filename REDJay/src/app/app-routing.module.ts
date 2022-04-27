4537e4c46154dc80a8dfc4e5d41a48388a5b4804
Rahmel Hill <rahmelhill@Rahmels-MacBook-Pro.local>
2022-04-26 23:47:32 -0400

    getalluser/putalluser update

--- REDJay/src/app/app-routing.module.ts
+++ REDJay/src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+// import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule, Routes } from '@angular/router';
-import { AppComponent } from './app.component';
+// import { AppComponent } from './app.component';
 import { AboutuspageComponent } from './aboutuspage/aboutuspage.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { InvPageComponent } from './inv-page/inv-page.component';
 import { LocalepageComponent } from './localepage/localepage.component';
-import { LoginpageComponent } from './loginpage/loginpage.component';
+// import { LoginpageComponent } from './loginpage/loginpage.component';
 
 
 const routes: Routes = [
@@ -14,7 +14,7 @@ const routes: Routes = [
   {path:'home',component:HomepageComponent},
   {path:'inventory',component:InvPageComponent},
   {path:'locale',component:LocalepageComponent},
-  {path:'login',component:LoginpageComponent},
+  // {path:'login',component:LoginpageComponent},
   {path:'about',component:AboutuspageComponent}
 ]; 
    // --this is where u put all ur components and url
--- REDJay/src/app/uploadpage/uploadpage.component.ts
+++ REDJay/src/app/uploadpage/uploadpage.component.ts
@@ -46,6 +46,89 @@ export class UploadpageComponent implements OnInit {
       });
 
       }
+      getAllUserJacketsCoat()
+      {
+      this._clothes.getUserJacketsCoat().subscribe((result) => { 
+        this.customersData = result
+        console.log(result)
+      });
+
+      }
+      getAllUserSneaker()
+      {
+      this._clothes.getUserSneaker().subscribe((result) => { 
+        this.customersData = result
+        console.log(result)
+      });
+
+      }
+
+      getAllUserSweats()
+      {
+      this._clothes.getUserSweats().subscribe((result) => { 
+        this.customersData = result
+        console.log(result)
+      });
+
+      }
+      getAllUserFullPieceSuit()
+      {
+      this._clothes.getUserFullPieceSuit().subscribe((result) => { 
+        this.customersData = result
+        console.log(result)
+      });
+
+      }
+      getAllUserJean()
+
+      {
+      this._clothes.  getUserJean().subscribe((result) => { 
+        this.customersData = result
+        console.log(result)
+      });
+    }
+          getAllUserSandals()
+          {
+          this._clothes.getUserSandals().subscribe((result) => { 
+            this.customersData = result
+            console.log(result)
+          });
+        }
+        getAllUserShirts()
+          {
+          this._clothes.getUserShirts().subscribe((result) => { 
+            this.customersData = result
+            console.log(result)
+          });
+        }
+        getAllUserSuitTop()
+          {
+          this._clothes.getUserSuitTop().subscribe((result) => { 
+            this.customersData = result
+            console.log(result)
+          });
+            }
+            getAllUserSuitBottom()
+        {
+        this._clothes.getUserSuitBottom().subscribe((result) => { 
+          this.customersData = result
+          console.log(result)
+        });
+      }
+      getAllUserTankTop()
+      {
+      this._clothes.getUserTankTop().subscribe((result) => { 
+        this.customersData = result
+        console.log(result)
+      });
+    }
+    getAllUserShorts()
+      {
+      this._clothes.getUserShorts().subscribe((result) => { 
+        this.customersData = result
+        console.log(result)
+      });
+      }
 
       //put
       editUserBoot(bootid:any) 
@@ -60,9 +143,106 @@ export class UploadpageComponent implements OnInit {
 
 
 
-      
+      editserFullPieceSuit(suitid:any) 
+      {
+
+      console.log(suitid)
+
+      this._clothes.approveUserFullPieceSuit(suitid).subscribe((result)=> {
+        console.log(result)
+      });
+      }
+
+
+      editUserJacketsCoat(jacketcoatid:any) 
+      {
+
+      console.log(jacketcoatid)
+
+      this._clothes.approveUserJacketsCoat(jacketcoatid).subscribe((result)=> {
+        console.log(result)
+      });
+      }
+
+
+      editUserJeans(jeansid:any) 
+      {
+
+      console.log(jeansid)
+
+      this._clothes.approveUserJeans(jeansid).subscribe((result)=> {
+        console.log(result)
+      });
+      }
+
+
+      editUserSandals(sandalsid:any) 
+      {
+
+      console.log(sandalsid)
+
+      this._clothes.approveUserSandals(sandalsid).subscribe((result)=> {
+        console.log(result)
+      });
+      }
+
+
+      editUserShirts(shirtsid:any) 
+      {
+
+      console.log(shirtsid)
+
+      this._clothes.approveUserShirts(shirtsid).subscribe((result)=> {
+        console.log(result)
+      });
+      }
+
+
+      editUserSneakers(sneakersid:any) 
+      {
+
+      console.log(sneakersid)
+
+      this._clothes.approveUserSneakers(sneakersid).subscribe((result)=> {
+        console.log(result)
+      });
+      }
+
+
+      editUserSuitTop(suittopid:any) 
+      {
+
+      console.log(suittopid)
+
+      this._clothes.approveUserSuitTop(suittopid).subscribe((result)=> {
+        console.log(result)
+      });
+      }
+
+
+      editUserSuitBottom(suitbottomid:any) 
+      {
+
+      console.log(suitbottomid)
+
+      this._clothes.approveUserSuitBottom(suitbottomid).subscribe((result)=> {
+        console.log(result)
+      });
+      }
+
+
+      editUserShorts(shortsid:any) 
+      {
+
+      console.log(shortsid)
+
+      this._clothes.approveUserShorts(shortsid).subscribe((result)=> {
+        console.log(result)
+      });
+      }
+
+
           
-        
         ngOnInit(): void {
        
        
